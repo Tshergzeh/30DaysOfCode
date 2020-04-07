b361935de93408f7a19e6d2bab4926a9575a906d
Tshergzeh <40037490+Tshergzeh@users.noreply.github.com>
2020-04-07 15:17:42 +0100

    Create day1.php

---
+++ day1.php
@@ -0,0 +1,15 @@
+<?php
+function NegOrPos(int $var)
+{
+    if ($var < 0) {
+        echo "Negative";
+    } elseif ($var == 0) {
+        echo "0: Unsigned";
+    }
+    else {
+        echo "Positive";
+    }
+    
+}
+NegOrPos(-9);
+?>
